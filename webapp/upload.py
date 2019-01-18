e1dbb0c6c912c9b8fdd8287c53e616fe30afe559
Dakad <3106338+Dakad@users.noreply.github.com>
2019-01-18 14:56:30 +0100

    Add UploadHandler

---
+++ webapp/upload.py
@@ -0,0 +1,78 @@
+#! /usr/bin/env python
+
+import os
+from datetime import datetime
+from random import choice
+from string import ascii_lowercase, digits
+from urllib.parse import unquote
+
+
+import tornado.web
+
+
+_UPLOAD_FILENAME_FORMAT = "%Y%m%d_%H%M%S_%f"
+
+
+class UploadHandler(tornado.web.RequestHandler):
+
+    def initialize(self, db, upload_folder,task_folder ):
+        self._db = db
+        self._upload_folder = upload_folder
+        self._task_folder = task_folder
+
+
+    def _clean_filename(self, filename):
+        """Remove any "evil" special characters
+
+        Arguments:
+            filename {string} -- The name of the file
+
+        Returns:
+            string -- Clean filename
+        """
+
+        idx = filename.rfind(".")
+        f_name = filename[:idx].replace(".", "")
+        f_name += str(datetime.now().time()).split('.')[1]
+        f_name += filename[idx:]
+        filename = f_name.replace("/", "")
+
+        return filename
+
+    @staticmethod
+    def random_name():
+        return ''.join(choice(ascii_lowercase + digits) for _ in range(10))
+
+    def get(self):
+        self.render("upload.html", title="Send me all pics !")
+
+    def post(self):
+        try:
+            upload_id = datetime.now().strftime(_UPLOAD_FILENAME_FORMAT)
+            uploads = self.request.files['uploads']
+
+            # TODO: Write upload user data in SQL DB
+
+            for idx, upload in enumerate(uploads):
+                up_ext = os.path.splitext(upload['filename'])[1]
+                # Just use the idx with 000,padded as name
+                upload_name = str(idx).zfill(3) + up_ext
+                upload_f_name = os.path.join(self._upload_folder, upload_name)
+                with open(upload_f_name, 'wb') as upload_file:
+                    upload_file.write(upload['body'])
+
+            # Write a task file for the pipeline
+            new_task = os.path.join(self._task_folder, upload_id)
+            open(new_task, 'a').close()
+        finally:
+            self.redirect('/upload/')
+
+
+@tornado.web.stream_request_body
+class UploadStreamHandler(UploadHandler):
+    def initialize(self):
+        self.bytes_read = 0
+    
+    def data_received(self, chunk):
+        pass
+
