11eea1c676846afecda41eeee133d537b22645e7
Dakad <3106338+Dakad@users.noreply.github.com>
2019-01-28 01:12:04 +0100

    Rename var names

--- webapp/upload.py
+++ webapp/upload.py
@@ -15,11 +15,10 @@ _UPLOAD_FILENAME_FORMAT = "%Y%m%d_%H%M%S_%f"
 
 class UploadHandler(tornado.web.RequestHandler):
 
-    def initialize(self, db, upload_folder,task_folder ):
+    def initialize(self, db, upload_dir, task_dir):
         self._db = db
-        self._upload_folder = upload_folder
-        self._task_folder = task_folder
-
+        self._upload_dir = upload_dir
+        self._task_dir = task_dir
 
     def _clean_filename(self, filename):
         """Remove any "evil" special characters
@@ -57,12 +56,12 @@ class UploadHandler(tornado.web.RequestHandler):
                 up_ext = os.path.splitext(upload['filename'])[1]
                 # Just use the idx with 000,padded as name
                 upload_name = str(idx).zfill(3) + up_ext
-                upload_f_name = os.path.join(self._upload_folder, upload_name)
+                upload_f_name = os.path.join(self._upload_dir, upload_name)
                 with open(upload_f_name, 'wb') as upload_file:
                     upload_file.write(upload['body'])
 
             # Write a task file for the pipeline
-            new_task = os.path.join(self._task_folder, upload_id)
+            new_task = os.path.join(self._task_dir, upload_id)
             open(new_task, 'a').close()
         finally:
             self.redirect('/upload/')
@@ -72,7 +71,6 @@ class UploadHandler(tornado.web.RequestHandler):
 class UploadStreamHandler(UploadHandler):
     def initialize(self):
         self.bytes_read = 0
-    
+
     def data_received(self, chunk):
         pass
-
